370b95eaf22b602ebe2a7baeb4651aa31c206bfb
hrdpaliwal93 <hardikpaliwal2015@gmail.com>
2026-06-28 18:49:00 +0530

    test

--- backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-const db = await mysql.createConnection({
-    host: "localhost",
-    "user": "root",
-    password: "hrdpaliwal",
-    database: "hardik"
-});
-console.log(await db.execute("show tables"));
-//# sourceMappingURL=index.js.map
